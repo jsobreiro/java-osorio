3a045cb0eca4952d6c3621b79ac8ce3d1a867ca8
jsobreiro <jasobreiro@up.edu.br>
2024-04-29 09:12:23 -0300

    Classe Cadastro criada

---
+++ Aula18/src/Cadastro.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Cadastro {
+
+    private static ArrayList<Personagem> listaPersonagens = new ArrayList<>();
+
+    public static void cadastrarPersonagem(Personagem personagem) {
+        listaPersonagens.add(personagem);
+    }
+
+    public static ArrayList<Personagem> listarPersonagens() {
+        return listaPersonagens;
+    }
+
+}
--- Aula18/src/Principal.java
+++ Aula18/src/Principal.java
@@ -1,35 +1,19 @@
 public class Principal {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Heroi heroi1 = new Heroi("Josepher, o Grande",
-                30, "Barbaro");
+                /*
+                 * TODO:
+                 * 
+                 * Criar uma classe Sistema, com métodos para: exibir menu, veririficar opçoes e
+                 * executar o sistema.
+                 * Chamar o método executar, da classe Sistema, dentro do método main, desta
+                 * classe
+                 * Principal.
+                 * Use a aula 16 como base para resolução desta atividade
+                 * 
+                 */
 
-        Vilao vilao1 = new Vilao();
-        Vilao vilao2 = new Vilao("Skull",
-                20, 2);
-
-        System.out.println("Herói 1:\n" + heroi1);
-        System.out.println("\nVilão1 :\n" + vilao1);
-        System.out.println("\nVilão2 :\n" + vilao2);
-
-        /*
-         * TODO:
-         * Apagar as linhas acima de teste
-         * Criar uma classe para cadastro de personagens (tanto heroi, quanto vilao)
-         * Esta classe deverá conter um arraylist estático para armazenar objetos do
-         * tipo Personagem.
-         * Criar método para cadastrar personagem e, por enquanto, listar todos os
-         * personagens.
-         * Criar uma classe Sistema, com métodos para: exibir menu, veririficar opçoes e
-         * executar o sistema.
-         * Chamar o método executar, da classe Sistema, dentro do método main, desta
-         * classe
-         * Principal.
-         * Use a aula 16 como base para resolução desta atividade
-         * 
-         */
-
-    }
+        }
 
 }
