3d7184fbf0f40c56df14225d434ac381490ff13b
jasobreiro <jasobreiro@up.edu.br>
2024-05-10 09:32:09 -0300

    Console adicionado ao projeto

--- Aula22/src/App.java
+++ Aula22/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println("Informe um valor inteiro: ");
+        int valor = Console.lerInt();
+
+        System.out.println("Valor digitado: " + valor);
+
     }
 }
