f4b374ed69a87093e46b427f787eee6a23436872
jasobreiro <jasobreiro@up.edu.br>
2024-05-17 10:05:34 -0300

    Lendo dados do arquivo e exibindo na tela

--- Aula24/src/App.java
+++ Aula24/src/App.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        lerJogosDoArrayList();
+    }
+
+    private static void salvarJogos() {
 
         Jogo jogo1 = new Jogo(1, "The Last of Us", "Acao/Aventura");
         Jogo jogo2 = new Jogo(2, "Super Mario World", "Plataforma");
@@ -9,7 +16,40 @@ public class App {
         GerenciadorJogo.salvarJogo(jogo2);
         GerenciadorJogo.salvarJogo(jogo3);
 
-        System.out.println("3 Jogos foram salvos no arquivo 'jogos.txt'");
+        System.out.println("\n3 Jogos foram salvos no arquivo 'jogos.txt'");
+    }
+
+    private static void lerJogosDoArquivo() {
+
+        System.out.println("\nJogos cadastrados no arquivo:");
+
+        try {
+
+            GerenciadorJogo.lerJogos();
+
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void lerJogosDoArrayList() {
+
+        System.out.println("\nJogos cadastrados:");
+
+        try {
+
+            for (Jogo tempJogo : GerenciadorJogo.getListaJogos()) {
+
+                System.out.println(tempJogo);
+
+            }
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+        }
 
     }
 }
--- Aula24/src/GerenciadorJogo.java
+++ Aula24/src/GerenciadorJogo.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public abstract class GerenciadorJogo {
 
@@ -16,6 +19,53 @@ public abstract class GerenciadorJogo {
         } catch (IOException e) {
             System.out.println("Houve um erro ao criar ou acessar o arquivo " + ARQUIVO);
         }
+
+    }
+
+    public static void lerJogos() throws IOException {
+
+        try (FileReader fReader = new FileReader(ARQUIVO);
+                BufferedReader bReader = new BufferedReader(fReader)) {
+
+            String linha;
+
+            while ((linha = bReader.readLine()) != null) {
+
+                System.out.println(linha);
+
+            }
+
+        }
+
+    }
+
+    public static ArrayList<Jogo> getListaJogos() throws IOException, Exception {
+
+        ArrayList<Jogo> listaJogos = new ArrayList<>();
+
+        try (FileReader fReader = new FileReader(ARQUIVO);
+                BufferedReader bReader = new BufferedReader(fReader)) {
+
+            String linha;
+
+            while ((linha = bReader.readLine()) != null) {
+
+                String[] dadosJogo = linha.split(", ");
+
+                Jogo jogo = new Jogo(Integer.parseInt(dadosJogo[0]),
+                        dadosJogo[1],
+                        dadosJogo[2]);
+
+                listaJogos.add(jogo);
+            }
+        }
+
+        if (listaJogos.isEmpty()) {
+
+            throw new Exception("\nNão há jogos cadastrados!");
+        }
+
+        return listaJogos;
 
     }
 
