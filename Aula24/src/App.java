11eba29e9479478ac338837cc6fb9efe934dc222
jasobreiro <jasobreiro@up.edu.br>
2024-05-17 10:34:24 -0300

    Exemplo 1 da aula 24 finalizado

--- Aula24/src/App.java
+++ Aula24/src/App.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        salvarJogos();
+        lerJogosDoArquivo();
         lerJogosDoArrayList();
     }
 
